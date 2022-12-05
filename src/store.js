4c757b4b5c320e3fd67a9deff271f85c25577904
tilenmiklavic <tm7321@student.uni-lj.si>
2022-12-05 08:27:56 +0100

    Delete firebase config files

--- src/store.js
+++ src/store.js
@@ -1,10 +1,1 @@
 import { writable } from "svelte/store";
-
-// let storedUser = localStorage.getItem("user");
-
-// export const user = writable(storedUser);
-
-// user.subscribe(value => {
-//     console.log("Value", value)
-//     localStorage.setItem("user", JSON.stringify(value));
-// });
